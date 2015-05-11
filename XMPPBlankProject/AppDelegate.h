3c706efeccf1084469dd8209bb2b0d2566e7e477
Talles Borges <talles.borges92@gmail.com>
2015-05-11 11:46:25 -0300

    Initial Commit

---
+++ XMPPBlankProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMPPBlankProject
+//
+//  Created by RVSLabs on 11/05/15.
+//  Copyright (c) 2015 RVSLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMPPBlankProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMPPBlankProject
+//
+//  Created by RVSLabs on 11/05/15.
+//  Copyright (c) 2015 RVSLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
